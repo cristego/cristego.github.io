19877a5bb84d006ba5aa235fa831b6567da6388b
Christian Popa <cristego@gmail.com>
2021-11-23 08:26:48 +0100

    test gulp post processing on netlify

---
+++ gulpfile.js
@@ -0,0 +1,24 @@
+const gulp = require('gulp');
+const {watch, series} = require('gulp');
+const clean_css = require('gulp-clean-css');
+
+function minify_css(f){
+     // Folder with files to minify
+     return gulp.src('assets/*.css')
+     //The method pipe() allow you to chain multiple tasks together 
+     //Execute the task to minify the files
+    .pipe(clean_css())
+    //I define the destination of the minified files with the method dest
+    .pipe(gulp.dest('css'));
+
+    f();
+}
+
+
+exports.minify_css = minify_css;
+
+//exports.default = series(minify_css, watch);
+exports.default = function() {
+    watch('assets/*.css', series(minify_css));
+}
+
