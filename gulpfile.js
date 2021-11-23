da2534cb607ac368cad068e5c926b8c65e62ba9d
Christian Popa <cristego@gmail.com>
2021-11-23 10:01:57 +0100

    bundle css with gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,11 @@
 const gulp = require('gulp');
 const {watch, series} = require('gulp');
-const concat_css = require ('gulp-concat-css');
+const concat_css = require('gulp-concat-css');
 const clean_css = require('gulp-clean-css');
 
 function minify_css(f){
      // Folder with files to minify
-     return gulp.src('_site/assets/*.css')
+     return gulp.src('_site/assets/**/*.css')
         //The method pipe() allow you to chain multiple tasks together
         //Bundle css files found in assets folder
         .pipe(concat_css("_site/core.css")) 
@@ -14,7 +14,7 @@ function minify_css(f){
         //Define the destination of the minified files with the method dest
         .pipe(gulp.dest('_site/css'));
 
-    f();
+        f();
 }
 
 
